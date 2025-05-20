5264c8e29f8068b3702063b467ce7a997e3ea5d8
Tuft <zac@tuft.co.nz>
2025-05-20 22:01:25 +1200

    - Ignore the performance hits (yes it will be bad with many chests) - Check for the chest existing in our index before adding to it + stop purging the list every frame. - Pull the chests into a straight forward object to hold some meta data.

--- Class1.cs
+++ Class1.cs
@@ -5,6 +5,7 @@ using BepInEx.Logging;
 using HarmonyLib;
 using System.Reflection;
 using UnityEngine;
+// ReSharper disable Unity.PerformanceCriticalCodeInvocation
 
 namespace ChestContents
 {
@@ -14,10 +15,11 @@ namespace ChestContents
         const string pluginGUID = "sticky.chestcontents";
         const string pluginName = "ChestContents";
         const string pluginVersion = "1.0.0";
-
         private readonly Harmony HarmonyInstance = new Harmony(pluginGUID);
-
         public static ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource(pluginName);
+        
+        private static List<Container> _chests = new List<Container>();
+        private static Dictionary<int, ChestInfo> _chestInfo = new Dictionary<int, ChestInfo>();
 
         public void Awake()
         {
@@ -31,15 +33,23 @@ namespace ChestContents
             if (Player.m_localPlayer != null)
             {
                 PopulateContainers();
+                // Flesh out container info
+                _chests.ForEach(chest =>
+                {
+                    ChestInfo ci = new ChestInfo(chest);
+                    if (!_chestInfo.ContainsKey(ci.InstanceID))
+                    {
+                        Main.logger.LogInfo("Added a chest (" + ci.InstanceID + ") with " + ci.Contents.Count + " items.");
+                        _chestInfo.Add(ci.InstanceID, ci);
+                    }
+                });
             }
         }
 
-        private static List<Container> _chests = new List<Container>();
         private static Collider[] _colliders = new Collider[10240];
 
         public void PopulateContainers()
         {
-            _chests.Clear();
 
             // Get all chests in the world
             int size = Physics.OverlapSphereNonAlloc(Player.m_localPlayer.transform.position, 30f, _colliders);
@@ -56,18 +66,18 @@ namespace ChestContents
                 Container container = collider.transform.gameObject.GetComponentInParent<Container>();
                 if (container)
                 {
+                    int instanceID = container.GetInstanceID();
+                    Container result = _chests.Find(x => x.GetInstanceID() == instanceID);
                     if(CheckContainerAccess(container, Player.m_localPlayer))
                     {
-                        // Add the container to the list
-                        _chests.Add(container);
+                        if (result == null)
+                        {
+                            // Add the container to the list if instance isn't already there
+                            _chests.Add(container);
+                        }
                     }
                 }
             }
-            // Check the user has permission to see the contents of the chests
-            
-
-            // Log the number of chests found
-            Main.logger.LogInfo($"Found {_chests.Count} chests.");
         }
 
         private static bool CheckContainerAccess(Container container, Player player)
@@ -83,4 +93,31 @@ namespace ChestContents
         [HarmonyPatch(typeof(Container), "CheckAccess")]
         public static bool RunCheckAccess(Container container, long playerID) => throw new NotImplementedException("This is a reverse patch, please use the original method instead.");
     }
+    
+    // Struct holding the chest position, an identifier and the contents
+    public struct ChestInfo
+    {
+        public Vector3 Position;
+        public int InstanceID;
+        public List<ItemDrop.ItemData> Contents;
+        public DateTime LastUpdated;
+
+        public ChestInfo(Vector3 position, int instanceID, List<ItemDrop.ItemData> contents)
+        {
+            Position = position;
+            InstanceID = instanceID;
+            Contents = contents;
+            LastUpdated = DateTime.Now;
+        }
+
+        public ChestInfo(Container container)
+        {
+            Position = container.transform.position;
+            InstanceID = container.GetInstanceID();
+            Contents = container.GetInventory().GetAllItemsInGridOrder();
+            LastUpdated = DateTime.Now;
+
+        }
+    }
+    
 }
